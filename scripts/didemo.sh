3be98e6a0806ecfe2a0be763c8b47364f86ba1d6
lhxxxmad <lhx7652@gmail.com>
2023-06-06 04:33:54 +0000

    update

---
+++ scripts/didemo.sh
@@ -0,0 +1,69 @@
+mkdir data
+cd data
+echo "download data"
+hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/data/didemo.zip 
+unzip didemo.zip  -d ./
+cd ..
+
+hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/EMCL-Net/tvr/models/ViT-B-32.pt
+mv ViT-B-32.pt ./tvr/models
+
+export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
+split_hosts=$(echo $ARNOLD_WORKER_HOSTS | tr ":" "\n")
+split_hosts=($split_hosts)
+
+DATA_PATH=./data/didemo
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+--master_addr ${ARNOLD_WORKER_0_HOST} \
+--master_port ${ARNOLD_WORKER_0_PORT} \
+main.py \
+--do_train 1 \
+--workers 8 \
+--n_display 1 \
+--epochs 5 \
+--lr 1e-4 \
+--coef_lr 1e-3 \
+--batch_size 128 \
+--batch_size_val 128 \
+--anno_path ${DATA_PATH}/ \
+--video_path ${DATA_PATH}/videos \
+--datatype didemo \
+--max_words 64 \
+--max_frames 64 \
+--video_framerate 1 \
+--output_dir outputs/didemo \
+--embd_mode wti \
+--do_gauss 1 \
+--video_mask_rate 0.1 \
+--text_mask_rate 0.1 \
+--temp_loss_weight 1.0 \
+--rec_loss_weight 1.0 \
+--ret_loss_weight 1.0
+
+echo "test model"
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+--master_addr ${ARNOLD_WORKER_0_HOST} \
+--master_port ${ARNOLD_WORKER_0_PORT} \
+main.py \
+--do_eval 1 \
+--workers 8 \
+--n_display 1 \
+--epochs 5 \
+--lr 1e-4 \
+--coef_lr 1e-3 \
+--batch_size 128 \
+--batch_size_val 128 \
+--anno_path ${DATA_PATH}/ \
+--video_path ${DATA_PATH}/videos \
+--datatype didemo \
+--max_words 64 \
+--max_frames 64 \
+--video_framerate 1 \
+--init_model outputs/didemo/best.bin \
+--output_dir outputs/didemo \
+--embd_mode wti \
+--do_gauss 1 \
+--video_mask_rate 0.1 \
+--text_mask_rate 0.1 
--- tvr/models/modeling.py
+++ tvr/models/modeling.py
@@ -474,14 +474,14 @@ class SLIP(nn.Module):
             # saliency token
             if gauss:
                 # text_weight = self.text_weight_fc(cross_text_feat).squeeze(2)  # B_t x N_t x D -> B_t x N_t
-                text_weight =  self.text_saliency_fc(cross_text_feat[:,-1])
-                video_weight =  self.video_saliency_fc(cross_video_feat[:,-1])
+                # text_weight =  self.text_saliency_fc(cross_text_feat[:,-1])
+                # video_weight =  self.video_saliency_fc(cross_video_feat[:,-1])
                 props = self.moment_fc(cross_video_feat[:,-1])
-                # cross_video_feat = cross_video_feat[:, : -1]
-                # cross_text_feat = cross_text_feat[:, : -1]
+                cross_video_feat = cross_video_feat[:, : -1]
+                cross_text_feat = cross_text_feat[:, : -1]
 
-                # text_weight = self.text_weight_fc(cross_text_feat).squeeze(2)  # B_t x N_t x D -> B_t x N_t
-                # video_weight = self.video_weight_fc(cross_video_feat).squeeze(2) # B_v x N_v x D -> B_v x N_v
+                text_weight = self.text_weight_fc(cross_text_feat).squeeze(2)  # B_t x N_t x D -> B_t x N_t
+                video_weight = self.video_weight_fc(cross_video_feat).squeeze(2) # B_v x N_v x D -> B_v x N_v
 
                 text_feat = text_feat[:, : -1]
                 video_feat = video_feat[:, : -1]
