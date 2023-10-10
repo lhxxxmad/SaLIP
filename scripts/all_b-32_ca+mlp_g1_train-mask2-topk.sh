a211414d4361942268109e227ceb567a6a88dc7c
chengxuxin <chengxuxin@bytedance.com>
2023-10-10 21:42:40 +0800

    fix DG bug

--- scripts/all_b-32_ca+mlp_g1_train-mask2-topk.sh
+++ scripts/all_b-32_ca+mlp_g1_train-mask2-topk.sh
@@ -229,6 +229,7 @@ main.py \
 --mask_mode topk
 
 echo "test msrvtt->didemo"
+DATA_PATH=./data/didemo
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 python3 -m torch.distributed.launch --nproc_per_node=8 \
 --master_addr ${ARNOLD_WORKER_0_HOST} \
@@ -260,6 +261,7 @@ main.py \
 --mask_mode topk
 
 echo "test msrvtt->lsmdc"
+DATA_PATH=./data/Clip_LSMDC
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 python3 -m torch.distributed.launch --nproc_per_node=8 \
 --master_addr ${ARNOLD_WORKER_0_HOST} \
