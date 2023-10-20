f4cb3651b93bdf598eec6e8848e2d70e81e79ab1
chengxuxin <chengxuxin@bytedance.com>
2023-10-20 23:44:31 +0800

    update sh

--- scripts/all_b-32_ca+mlp_g1_train-mask-mean.sh
+++ scripts/all_b-32_ca+mlp_g1_train-mask-mean.sh
@@ -3,15 +3,15 @@ cd data
 # echo "download anet"
 # hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/data/anet.tar.gz
 # tar -zxvf anet.tar.gz
-# echo "download didemo"
-# hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/data/didemo.zip 
-# unzip didemo.zip  -d ./
+echo "download didemo"
+hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/data/didemo.zip 
+unzip didemo.zip  -d ./
 echo "download msrvtt"
 hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/data/MSRVTT.tar.gz
 tar -zxvf MSRVTT.tar.gz
-# echo "download lsmdc"
-# hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/data/LSMDC.zip
-# unzip LSMDC.zip
+echo "download lsmdc"
+hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/data/LSMDC.zip
+unzip LSMDC.zip
 cd ..
 
 hdfs dfs -get hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/EMCL-Net/tvr/models/ViT-B-32.pt
@@ -91,71 +91,71 @@ split_hosts=($split_hosts)
 # --mask_mode mean
 
 
-# echo "train didemo"
-# DATA_PATH=./data/didemo
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-# python3 -m torch.distributed.launch --nproc_per_node=8 \
-# --master_addr ${ARNOLD_WORKER_0_HOST} \
-# --master_port ${ARNOLD_WORKER_0_PORT} \
-# main.py \
-# --do_train 1 \
-# --workers 8 \
-# --n_display 1 \
-# --epochs 5 \
-# --lr 1e-4 \
-# --coef_lr 1e-3 \
-# --batch_size 128 \
-# --batch_size_val 128 \
-# --anno_path ${DATA_PATH}/ \
-# --video_path ${DATA_PATH}/videos \
-# --datatype didemo \
-# --max_words 64 \
-# --max_frames 64 \
-# --video_framerate 1 \
-# --output_dir outputs/didemo_ViT-B-32 \
-# --embd_mode wti \
-# --do_gauss 1 \
-# --video_mask_rate 0.8 \
-# --text_mask_rate 0.8 \
-# --temp_loss_weight 1.0 \
-# --rec_loss_weight 1.0 \
-# --ret_loss_weight 1.0 \
-# --sal_predictor ca+mlp \
-# --training_mask 1 \
-# --interaction_mask 0.8 \
-# --mask_mode mean
+echo "train didemo"
+DATA_PATH=./data/didemo
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+--master_addr ${ARNOLD_WORKER_0_HOST} \
+--master_port ${ARNOLD_WORKER_0_PORT} \
+main.py \
+--do_train 1 \
+--workers 8 \
+--n_display 1 \
+--epochs 5 \
+--lr 1e-4 \
+--coef_lr 1e-3 \
+--batch_size 128 \
+--batch_size_val 128 \
+--anno_path ${DATA_PATH}/ \
+--video_path ${DATA_PATH}/videos \
+--datatype didemo \
+--max_words 64 \
+--max_frames 64 \
+--video_framerate 1 \
+--output_dir outputs/didemo_ViT-B-32 \
+--embd_mode wti \
+--do_gauss 1 \
+--video_mask_rate 0.8 \
+--text_mask_rate 0.8 \
+--temp_loss_weight 1.0 \
+--rec_loss_weight 1.0 \
+--ret_loss_weight 1.0 \
+--sal_predictor ca+mlp \
+--training_mask 1 \
+--interaction_mask 0.8 \
+--mask_mode mean
 
 
-# echo "test didemo"
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-# python3 -m torch.distributed.launch --nproc_per_node=8 \
-# --master_addr ${ARNOLD_WORKER_0_HOST} \
-# --master_port ${ARNOLD_WORKER_0_PORT} \
-# main.py \
-# --do_eval 1 \
-# --workers 8 \
-# --n_display 1 \
-# --epochs 5 \
-# --lr 1e-4 \
-# --coef_lr 1e-3 \
-# --batch_size 128 \
-# --batch_size_val 128 \
-# --anno_path ${DATA_PATH}/ \
-# --video_path ${DATA_PATH}/videos \
-# --datatype didemo \
-# --max_words 64 \
-# --max_frames 64 \
-# --video_framerate 1 \
-# --init_model outputs/didemo_ViT-B-32/best.bin \
-# --output_dir outputs/didemo_ViT-B-32 \
-# --embd_mode wti \
-# --do_gauss 1 \
-# --video_mask_rate 0.8 \
-# --text_mask_rate 0.8 \
-# --sal_predictor ca+mlp \
-# --training_mask 1 \
-# --interaction_mask 0.8 \
-# --mask_mode mean
+echo "test didemo"
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+--master_addr ${ARNOLD_WORKER_0_HOST} \
+--master_port ${ARNOLD_WORKER_0_PORT} \
+main.py \
+--do_eval 1 \
+--workers 8 \
+--n_display 1 \
+--epochs 5 \
+--lr 1e-4 \
+--coef_lr 1e-3 \
+--batch_size 128 \
+--batch_size_val 128 \
+--anno_path ${DATA_PATH}/ \
+--video_path ${DATA_PATH}/videos \
+--datatype didemo \
+--max_words 64 \
+--max_frames 64 \
+--video_framerate 1 \
+--init_model outputs/didemo_ViT-B-32/best.bin \
+--output_dir outputs/didemo_ViT-B-32 \
+--embd_mode wti \
+--do_gauss 1 \
+--video_mask_rate 0.8 \
+--text_mask_rate 0.8 \
+--sal_predictor ca+mlp \
+--training_mask 1 \
+--interaction_mask 0.8 \
+--mask_mode mean
 
 
 echo "train msrvtt"
@@ -294,70 +294,70 @@ main.py \
 # --interaction_mask 0.8 \
 # --mask_mode mean
 
-# echo "train lsmdc"
-# DATA_PATH=./data/Clip_LSMDC
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-# python3 -m torch.distributed.launch --nproc_per_node=8 \
-# --master_addr ${ARNOLD_WORKER_0_HOST} \
-# --master_port ${ARNOLD_WORKER_0_PORT} \
-# main.py \
-# --do_train 1 \
-# --workers 8 \
-# --n_display 5 \
-# --epochs 5 \
-# --lr 1e-4 \
-# --coef_lr 1e-3 \
-# --batch_size 128 \
-# --batch_size_val 128 \
-# --anno_path ${DATA_PATH}/ \
-# --video_path ${DATA_PATH}/LSMDC_Videos \
-# --datatype lsmdc \
-# --max_words 32 \
-# --max_frames 12 \
-# --video_framerate 1 \
-# --output_dir outputs/lsmdc_ViT-B-32 \
-# --embd_mode wti \
-# --do_gauss 1 \
-# --video_mask_rate 0.8 \
-# --text_mask_rate 0.8 \
-# --rec_trans_num_layers1 4 \
-# --rec_trans_num_layers2 4 \
-# --sal_predictor ca+mlp \
-# --training_mask 1 \
-# --interaction_mask 0.8 \
-# --mask_mode mean
+echo "train lsmdc"
+DATA_PATH=./data/Clip_LSMDC
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+--master_addr ${ARNOLD_WORKER_0_HOST} \
+--master_port ${ARNOLD_WORKER_0_PORT} \
+main.py \
+--do_train 1 \
+--workers 8 \
+--n_display 5 \
+--epochs 5 \
+--lr 1e-4 \
+--coef_lr 1e-3 \
+--batch_size 128 \
+--batch_size_val 128 \
+--anno_path ${DATA_PATH}/ \
+--video_path ${DATA_PATH}/LSMDC_Videos \
+--datatype lsmdc \
+--max_words 32 \
+--max_frames 12 \
+--video_framerate 1 \
+--output_dir outputs/lsmdc_ViT-B-32 \
+--embd_mode wti \
+--do_gauss 1 \
+--video_mask_rate 0.8 \
+--text_mask_rate 0.8 \
+--rec_trans_num_layers1 4 \
+--rec_trans_num_layers2 4 \
+--sal_predictor ca+mlp \
+--training_mask 1 \
+--interaction_mask 0.8 \
+--mask_mode mean
 
-# echo "test lsmdc"
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-# python3 -m torch.distributed.launch --nproc_per_node=8 \
-# --master_addr ${ARNOLD_WORKER_0_HOST} \
-# --master_port ${ARNOLD_WORKER_0_PORT} \
-# main.py \
-# --do_eval 1 \
-# --workers 8 \
-# --n_display 5 \
-# --epochs 5 \
-# --lr 1e-4 \
-# --coef_lr 1e-3 \
-# --batch_size 128 \
-# --batch_size_val 128 \
-# --anno_path ${DATA_PATH}/ \
-# --video_path ${DATA_PATH}/LSMDC_Videos \
-# --datatype lsmdc \
-# --max_words 32 \
-# --max_frames 12 \
-# --video_framerate 1 \
-# --init_model outputs/lsmdc_ViT-B-32/best.bin \
-# --output_dir outputs/lsmdc_ViT-B-32 \
-# --embd_mode wti \
-# --do_gauss 1 \
-# --video_mask_rate 0.8 \
-# --text_mask_rate 0.8 \
-# --rec_trans_num_layers1 4 \
-# --rec_trans_num_layers2 4 \
-# --sal_predictor ca+mlp \
-# --training_mask 1 \
-# --interaction_mask 0.8 \
-# --mask_mode mean
+echo "test lsmdc"
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+python3 -m torch.distributed.launch --nproc_per_node=8 \
+--master_addr ${ARNOLD_WORKER_0_HOST} \
+--master_port ${ARNOLD_WORKER_0_PORT} \
+main.py \
+--do_eval 1 \
+--workers 8 \
+--n_display 5 \
+--epochs 5 \
+--lr 1e-4 \
+--coef_lr 1e-3 \
+--batch_size 128 \
+--batch_size_val 128 \
+--anno_path ${DATA_PATH}/ \
+--video_path ${DATA_PATH}/LSMDC_Videos \
+--datatype lsmdc \
+--max_words 32 \
+--max_frames 12 \
+--video_framerate 1 \
+--init_model outputs/lsmdc_ViT-B-32/best.bin \
+--output_dir outputs/lsmdc_ViT-B-32 \
+--embd_mode wti \
+--do_gauss 1 \
+--video_mask_rate 0.8 \
+--text_mask_rate 0.8 \
+--rec_trans_num_layers1 4 \
+--rec_trans_num_layers2 4 \
+--sal_predictor ca+mlp \
+--training_mask 1 \
+--interaction_mask 0.8 \
+--mask_mode mean
 
 hdfs dfs -put -f outputs hdfs://haruna/home/byte_arnold_lq_mlnlc/user/chengxuxin/lhx/VL/SaLIP_model
--- scripts/run.sh
+++ scripts/run.sh
@@ -7,36 +7,37 @@ export PYTHONWARNINGS='ignore:semaphore_tracker:UserWarning'
 # git remote set-url <your_url> https://<your_token>@github.com/<username>/<repo>.git
 
 # MSRVTT --do_train 1 \
-CUDA_VISIBLE_DEVICES=0 \
-python3 -m torch.distributed.launch --nproc_per_node=1 \
---master_addr ${ARNOLD_WORKER_0_HOST} \
---master_port ${ARNOLD_WORKER_0_PORT} \
-main.py \
---do_train 1 \
---workers 0 \
---n_display 50 \
---epochs 5 \
---lr 1e-4 \
---coef_lr 1e-3 \
---batch_size 8 \
---batch_size_val 128 \
---anno_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/MSRVTT/msrvtt_data \
---video_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/MSRVTT/MSRVTT_Videos \
---datatype msrvtt \
---max_words 32 \
---max_frames 12 \
---video_framerate 1 \
---output_dir outputs/msrvtt \
---embd_mode emcl \
---do_gauss 1 \
---video_mask_rate 0.7 \
---text_mask_rate 0.7 \
---temp_loss_weight 1.0 \
---rec_loss_weight 1.0 \
---ret_loss_weight 1.0 \
---sal_predictor ca+mlp \
---training_mask 1 \
---mask_mode mean
+# CUDA_VISIBLE_DEVICES=0 \
+# python3 -m torch.distributed.launch --nproc_per_node=1 \
+# --master_addr ${ARNOLD_WORKER_0_HOST} \
+# --master_port ${ARNOLD_WORKER_0_PORT} \
+# main.py \
+# --do_eval 1 \
+# --workers 0 \
+# --n_display 50 \
+# --epochs 5 \
+# --lr 1e-4 \
+# --coef_lr 1e-3 \
+# --batch_size 8 \
+# --batch_size_val 128 \
+# --anno_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/MSRVTT/msrvtt_data \
+# --video_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/MSRVTT/MSRVTT_Videos \
+# --datatype msrvtt \
+# --max_words 32 \
+# --max_frames 12 \
+# --video_framerate 1 \
+# --output_dir outputs/msrvtt \
+# --embd_mode wti \
+# --do_gauss 1 \
+# --video_mask_rate 0.7 \
+# --text_mask_rate 0.7 \
+# --temp_loss_weight 1.0 \
+# --rec_loss_weight 1.0 \
+# --ret_loss_weight 1.0 \
+# --sal_predictor ca+mlp \
+# --training_mask 1 \
+# --mask_mode mean \
+# --init_model /mnt/bd/cxx-dataset/SaLIP/salip_mdoel/outputs/msrvtt_ViT-B-32/best.bin
 
 
 # 
@@ -117,28 +118,37 @@ main.py \
 # --do_gauss 0 \
 
 # DiDeMo
-# CUDA_VISIBLE_DEVICES=0 \
-# python3 -m torch.distributed.launch --nproc_per_node=1 \
-# --master_addr ${split_hosts[0]} \
-# --master_port ${split_hosts[1]} \
-# main.py \
-# --do_train 1 \
-# --workers 0 \
-# --n_display 50 \
-# --epochs 5 \
-# --lr 1e-4 \
-# --coef_lr 1e-3 \
-# --batch_size 8 \
-# --batch_size_val 8 \
-# --anno_path /mnt/bd/cxx-dataset/CLIP4Clip/data/DiDeMo/data \
-# --video_path /mnt/bd/cxx-dataset/CLIP4Clip/data/DiDeMo/videos \
-# --datatype didemo \
-# --max_words 64 \
-# --max_frames 64 \
-# --video_framerate 1 \
-# --output_dir outputs/didemo \
-# --embd_mode wti \
-# --do_gauss 0 \
+CUDA_VISIBLE_DEVICES=0,1 \
+python3 -m torch.distributed.launch --nproc_per_node=2 \
+--master_addr ${ARNOLD_WORKER_0_HOST} \
+--master_port ${ARNOLD_WORKER_0_PORT} \
+main.py \
+--do_train 1 \
+--workers 0 \
+--n_display 50 \
+--epochs 5 \
+--lr 1e-4 \
+--coef_lr 1e-3 \
+--batch_size 128 \
+--batch_size_val 128 \
+--anno_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/DiDeMo/data \
+--video_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/DiDeMo/videos \
+--datatype didemo \
+--max_words 32 \
+--max_frames 12 \
+--video_framerate 1 \
+--output_dir outputs/didemo \
+--embd_mode wti \
+--do_gauss 1 \
+--video_mask_rate 0.7 \
+--text_mask_rate 0.7 \
+--temp_loss_weight 1.0 \
+--rec_loss_weight 1.0 \
+--ret_loss_weight 1.0 \
+--sal_predictor ca+mlp \
+--training_mask 1 \
+--mask_mode mean \
+# --init_model /mnt/bd/cxx-dataset/SaLIP/SaLIP_model/outputs/msrvtt_ViT-B-32/best.bin
 
 # anet
 # CUDA_VISIBLE_DEVICES=0 \
@@ -173,7 +183,7 @@ main.py \
 # --master_addr ${ARNOLD_WORKER_0_HOST} \
 # --master_port ${ARNOLD_WORKER_0_PORT} \
 # main.py \
-# --do_train 1 \
+# --do_eval 1 \
 # --workers 0 \
 # --n_display 10 \
 # --epochs 5 \
@@ -181,8 +191,8 @@ main.py \
 # --coef_lr 1e-3 \
 # --batch_size 128 \
 # --batch_size_val 128 \
-# --anno_path /mnt/bd/cxx-dataset/CLIP4Clip/data/LSMDC/Clip_LSMDC \
-# --video_path /mnt/bd/cxx-dataset/CLIP4Clip/data/LSMDC/Clip_LSMDC/LSMDC_Videos \
+# --anno_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/LSMDC/Clip_LSMDC \
+# --video_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/LSMDC/Clip_LSMDC/LSMDC_Videos \
 # --datatype lsmdc \
 # --max_words 32 \
 # --max_frames 12 \
@@ -190,7 +200,8 @@ main.py \
 # --output_dir outputs/lsmdc \
 # --embd_mode wti \
 # --do_gauss 1 \
-# --init_model /mnt/bd/cxx-dataset/EMCL-Net/best_outputs/best.bin
+# --mask_mode mean \
+# --init_model /mnt/bd/cxx-dataset/SaLIP/SaLIP_model/outputs/msrvtt_ViT-B-32/best.bin
 
 
 # CUDA_VISIBLE_DEVICES=0 \
@@ -199,21 +210,28 @@ main.py \
 # --master_port ${ARNOLD_WORKER_0_PORT} \
 # main.py \
 # --do_eval 1 \
-# --workers 0 \
+# --workers 8 \
 # --n_display 50 \
 # --epochs 5 \
 # --lr 1e-4 \
 # --coef_lr 1e-3 \
 # --batch_size 8 \
-# --batch_size_val 8 \
-# --anno_path /mnt/bd/cxx-dataset/CLIP4Clip/data/activitynet/anet \
-# --video_path /mnt/bd/cxx-dataset/CLIP4Clip/data/activitynet/anet/Activity_Videos \
+# --batch_size_val 128 \
+# --anno_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/activitynet/anet \
+# --video_path /mnt/bd/cxx-dataset/CLIP4Clip_original/data/activitynet/anet/Activity_Videos \
 # --datatype activity \
-# --max_words 64 \
-# --max_frames 64 \
+# --max_words 32 \
+# --max_frames 12 \
 # --video_framerate 1 \
 # --output_dir outputs/activity \
 # --embd_mode wti \
 # --do_gauss 1 \
-# --num_props 3 \
-# --init_model /mnt/bd/cxx-third/GTVR/outputs/best/best.bin
+# --video_mask_rate 0.7 \
+# --text_mask_rate 0.7 \
+# --temp_loss_weight 1.0 \
+# --rec_loss_weight 1.0 \
+# --ret_loss_weight 1.0 \
+# --sal_predictor ca+mlp \
+# --training_mask 1 \
+# --mask_mode mean \
+# --init_model /mnt/bd/cxx-dataset/SaLIP/SaLIP_model/outputs/msrvtt_ViT-B-32/best.bin
