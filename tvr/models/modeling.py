f05a84d568c33681aeb0256688e9a643562e98a1
chengxuxin <chengxuxin@bytedance.com>
2023-10-29 23:22:35 +0800

    retrieve_logits0 = sim_ot

--- tvr/models/modeling.py
+++ tvr/models/modeling.py
@@ -32,6 +32,57 @@ class ResidualLinear(nn.Module):
         x = x + self.fc_relu(x)
         return x
 
+class SinkhornDistance(torch.nn.Module):
+    r"""
+        Given two empirical measures each with :math:`P_1` locations
+        :math:`x\in\mathbb{R}^{D_1}` and :math:`P_2` locations :math:`y\in\mathbb{R}^{D_2}`,
+        outputs an approximation of the regularized OT cost for point clouds.
+        Args:
+        eps (float): regularization coefficient
+        max_iter (int): maximum number of Sinkhorn iterations
+        reduction (string, optional): Specifies the reduction to apply to the output:
+        'none' | 'mean' | 'sum'. 'none': no reduction will be applied,
+        'mean': the sum of the output will be divided by the number of
+        elements in the output, 'sum': the output will be summed. Default: 'none'
+        Shape:
+            - Input: :math:`(N, P_1, D_1)`, :math:`(N, P_2, D_2)`
+            - Output: :math:`(N)` or :math:`()`, depending on `reduction`
+    """
+
+    def __init__(self, eps=1e-3, max_iter=100, reduction='none'):
+        super(SinkhornDistance, self).__init__()
+        self.eps = eps
+        self.max_iter = max_iter
+        self.reduction = reduction
+
+    def forward(self, mu, nu, C):
+        u = torch.ones_like(mu)
+        v = torch.ones_like(nu)
+
+        # Sinkhorn iterations
+        for i in range(self.max_iter):
+            v = self.eps * \
+                (torch.log(
+                    nu + 1e-8) - torch.logsumexp(self.M(C, u, v).transpose(-2, -1), dim=-1)) + v
+            u = self.eps * \
+                (torch.log(
+                    mu + 1e-8) - torch.logsumexp(self.M(C, u, v), dim=-1)) + u
+
+        U, V = u, v
+        # Transport plan pi = diag(a)*K*diag(b)
+        pi = torch.exp(
+            self.M(C, U, V)).detach()
+        # Sinkhorn distance
+        cost = torch.sum(
+            pi * C, dim=(-2, -1))
+        return cost, pi
+
+    def M(self, C, u, v):
+        '''
+        "Modified cost for logarithmic updates"
+        "$M_{ij} = (-c_{ij} + u_i + v_j) / epsilon$"
+        '''
+        return (-C + u.unsqueeze(-1) + v.unsqueeze(-2)) / self.eps
 
 class SLIP(nn.Module):
     def __init__(self, config):
@@ -226,7 +277,7 @@ class SLIP(nn.Module):
                         if num_layer < config.num_hidden_layers:
                             new_state_dict[key.replace("transformer.", "transformerClip.")] = val.clone()
                             continue
-
+        self.sinkhorn = SinkhornDistance()
         self.load_state_dict(new_state_dict, strict=False)  # only update new state (seqTransf/seqLSTM/tightTransf)
         ## <=== End of initialization trick
 
@@ -757,9 +808,12 @@ class SLIP(nn.Module):
 
             retrieve_logits0 = torch.einsum('abtv,bv->abtv', [retrieve_logits0, video_mask.squeeze(-1)])
             retrieve_logits1 = torch.einsum('abtv,bv->abtv', [retrieve_logits1, video_mask1.squeeze(-1)])
+            # import ot
+            # ot.sinkhorn(text_weight,video_weight,retrieve_logits,1.0,method='sinkhorn',numItermax=100, epsilon0=1e-6)
+            sim_ot = self.get_ot_sim(retrieve_logits0, text_weight, video_weight)
             # pdb.set_trace()
-            sim_ot = self.get_ot_sim(retrieve_logits0)
             # retrieve_logits0 = retrieve_logits0 + sim_ot
+            retrieve_logits0 = sim_ot
             # retrieve_logits2 = torch.einsum('abtv,bv->abtv', [retrieve_logits, video_mask2.squeeze(-1)])
             text_sum = text_mask.sum(-1)
             video_sum = video_mask.sum(-1)
@@ -835,11 +889,14 @@ class SLIP(nn.Module):
         if retrieve_logits1 is None:
             retrieve_logits1 = retrieve_logits
         # retrieve_logits = self.get_marginal_loss(retrieve_logits)
-        retrieve_logits1 = sim_ot
+        # retrieve_logits1 = sim_ot
         return retrieve_logits, retrieve_logits.T, retrieve_logits1, retrieve_logits1.T, text_weight, video_weight, props
         # return retrieve_logits, retrieve_logits.T, props
 
-    def get_ot_sim(self, sim):
+    def get_ot_sim(self, sim, text_weight, video_weight):
+        # sim = torch.einsum('abtv,at->abtv', [sim, text_weight])
+        # sim = torch.einsum('abtv,bv->abtv', [sim, video_weight])
+        
         a,b,t,v = sim.shape
         sim = sim.contiguous().view(a*b,t,v)
         # sim = sim.permute(2,0,1)
@@ -851,8 +908,10 @@ class SLIP(nn.Module):
             KK = torch.exp(-wdist / self.eps)
             T = self.Sinkhorn(KK,xx,yy)
         out = T * sim
+        out = out.contiguous().view(a,b,t,v)
         # out = out.permute(1,2,0)
-        return out.contiguous().view(a,b,t,v).sum(dim=[2,3])
+        # return out.contiguous().view(a,b,t,v).sum(dim=[2,3])
+        return out
         
     def Sinkhorn(self, K, u, v):
         r = torch.ones_like(u)
